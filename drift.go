573a7d44442694a6184a1707e85e8b5652cb2558
Antonio Iubatti <aiubatti@edgelab.ch>
2023-05-16 09:57:14 +0200

    chore: drift (#5)

---
+++ drift.go
@@ -0,0 +1,23 @@
+package main
+
+type Drift interface {
+	Value(s, t float64) float64
+}
+
+type DriftFunc func(s, t float64) float64
+
+var _ Drift = DriftFunc(nil)
+
+func (f DriftFunc) Value(s, t float64) float64 {
+	return f(s, t)
+}
+
+func NewConstantDrift(c float64) DriftFunc {
+	return func(_, _ float64) float64 {
+		return c
+	}
+}
+
+func NewZeroDrift() DriftFunc {
+	return NewConstantDrift(0.0)
+}
